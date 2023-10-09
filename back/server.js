0582e4ab078a678912d81f2610d96a926900994a
gg_ <89791105+ggvhs@users.noreply.github.com>
2023-10-08 22:42:56 -0400

    event listener added when an mp3 file is submitted/ get route added in server

--- back/server.js
+++ back/server.js
@@ -1,1 +1,11 @@
-console.log('hello world')
+import express from 'express'
+
+const app = express()
+
+app.use(express.static('front'))
+
+app.get('s3Url', (req,res) =>{
+    
+})
+
+app.listen(3000, () => console.log('listening on port 3000'))
