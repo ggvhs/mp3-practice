bdfdc204146771e9f92c9d0b5d87bad6195115dc
gg_ <89791105+ggvhs@users.noreply.github.com>
2023-10-09 15:55:01 -0400

    restarted project

--- front/index.js
+++
@@ -1,7 +0,0 @@
-const mp3Form = document.querySelector('#mp3Form')
-const mp3Input = document.querySelector('#mp3Input')
-
-mp3Form.addEventListener("submit", async event =>{
-    event.preventDefault()
-    const file = imageInput.files[0]
-})
